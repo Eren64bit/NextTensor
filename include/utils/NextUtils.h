05519907f4410e11ad5e2d01e1763761c0b21193
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-09-21 22:53:38 +0300

    Iscontiguous and TypeToDType functions

--- include/utils/NextUtils.h
+++ include/utils/NextUtils.h
@@ -4,6 +4,8 @@
 
 #pragma once
 #include <vector>
+#include <stdexcept>
+#include "DType.h"
 
 namespace Next {
     [[nodiscard]] inline  std::vector<size_t> ComputeStrides(const std::vector<size_t>& strides) {
@@ -24,8 +26,8 @@ namespace Next {
 
     [[nodiscard]] inline size_t FlattenIndex(const std::vector<size_t>& strides, std::vector<size_t>& indices) {
         size_t FlattenIndex = 0;
-        for (size_t i = 0; i < stride.size(); i++) {
-            FlattenIndex += stride[i] * indices[i];
+        for (size_t i = 0; i < strides.size(); i++) {
+            FlattenIndex += strides[i] * indices[i];
         }
         return FlattenIndex;
     }
@@ -38,4 +40,53 @@ namespace Next {
         }
         return result;
     }
+
+    [[nodiscard]] inline bool IsContiguous(const std::vector<size_t>& shape, const std::vector<size_t>& strides) {
+
+        if (shape.size() != strides.size()) throw std::invalid_argument("Shape and Strides Ranks must be match");
+
+        if (strides.back() != 1) throw std::invalid_argument("Last element of stride must be one.");
+
+        if (shape.empty()) return true;
+
+        for (int i = (int)strides.size() - 1; i >= 1; i--) {
+            if (strides[i-1] != strides[i] * shape[i]) return false;
+        }
+        return true;
+    }
+
+    template <typename T>
+    struct TypeToDType {
+        static constexpr DType value = DType::UNKNOWN;
+    };
+
+    template <>
+    struct TypeToDType<float> {
+        static constexpr DType value = DType::FLOAT32;
+    };
+
+    template <>
+    struct  TypeToDType<double> {
+        static constexpr DType value = DType::FLOAT64;
+    };
+
+    template <>
+    struct  TypeToDType<int32_t> {
+        static constexpr DType value = DType::INT32;
+    };
+
+    template <>
+    struct TypeToDType<int64_t> {
+        static constexpr DType value = DType::INT64;
+    };
+
+    template<>
+    struct TypeToDType<uint8_t> {
+        static constexpr DType value = DType::UINT8;
+    };
+
+    template<>
+    struct TypeToDType<bool> {
+        static constexpr DType value = DType::BOOL;
+    };
 }
