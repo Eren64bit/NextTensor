caa52ab2e9acf2b69ef9151d1f05868711448f77
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-09-21 22:52:09 +0300

    Added DType for Tensor Data types

---
+++ include/utils/DType.h
@@ -0,0 +1,17 @@
+//
+// Created by eren on 9/21/25.
+//
+
+#pragma once
+
+namespace Next {
+    enum class DType {
+        UNKNOWN,
+        FLOAT32,
+        FLOAT64,
+        INT32,
+        INT64,
+        UINT8,
+        BOOL
+    };
+}
